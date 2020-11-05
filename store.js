12b8ca9a8e4599fa77872b7e4f8902e6e7d14129
unknown <joshua.avornyo@amalitech.org>
2020-11-05 16:46:55 +0000

    added new features in the cart

--- store.js
+++ store.js
@@ -50,6 +50,37 @@ for(var i=0 ; i<removeCartItemButtons.length;i++){
      var title= shopItem.getElementsByClassName('shop-item-title')[0].innerText;
      var price= shopItem.getElementsByClassName('shop-item-price')[0].innerText;
      var imageSrc=shopItem.getElementsByClassName('shop-item-image')[0].src;
+     console.log(title,price,imageSrc);
+     addItemToCart(title,price,imageSrc);
+     updateCartTotal();
+ }
+
+ // ADD ITEMS TO CART
+ function addItemToCart(title,price,imageSrc){
+      var cartRow=document.createElement('div')
+      cartRow.classList.add('cart-row');
+      var cartItems= document.getElementsByClassName('cart-items')[0]
+      var cartItemNames= cartItems.getElementsByClassName('cart-item-title');
+      for(var i=0;i<cartItemNames.length;i++){
+          if(cartItemNames[i].innerText==title){
+              alert('This item has already been added to the list')
+              return;
+          }
+      }
+      var cartRowContents = `
+      <div class="cart-item cart-column">
+                        <img class="cart-item-image" src=" ${imageSrc}" width="100" height="100">
+                        <span class="cart-item-title">${title} </span>
+                    </div>
+                    <span class="cart-price cart-column">${price}</span>
+                    <div class="cart-quantity cart-column">
+                        <input class="cart-quantity-input" type="number" value="2">
+                        <button class="btn btn-danger" type="button">REMOVE</button>
+                    </div>`
+      cartRow.innerHTML=cartRowContents;
+      cartItems.append(cartRow);
+      cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click',removeCartItem);
+      cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change',quanityChanged);
  }
 
 //TO CALCULATE THE TOTAL PRICE OF ITEMS IN THE CART
